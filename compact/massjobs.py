50095cec4e15449e41fae05145c35f53aa533308
Aditya Agarwal <shiva@Adityas-MacBook-Air.local>
2025-08-30 12:10:27 -0400

    python file to produce scripts

--- compact/massjobs.py
+++ compact/massjobs.py
@@ -1,14 +1,12 @@
 from array import *
 import argparse
+import os
 
 
-# python massjobs.py -g DualTestBeam -N 500 -d 0 -o 2 -c /data/users/eno/CalVision/dd4hep/stuff4stuff/DualTestBeam/compact/ -w /data/users/eno/CalVision/dd4hep/stuff4stuff/DualTestBeam/compact/output/ -o 2 -s /data/users/eno/CalVision/dd4hep/stuff4stuff/DualTestBeam/compact/jobs/
+# python massjobs.py -g DualTestBeam -N 500 -d 0 -o 2
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-g", "--geometry", help="geometry code")
-argParser.add_argument("-w", "--write", help="where to write")
-argParser.add_argument("-s", "--script", help="where to put scripts")
-argParser.add_argument("-c", "--cdarea", help="where to run (compact area)")
 
 
 argParser.add_argument("-N", "--number", help="number to make")
@@ -22,20 +20,17 @@ print("args.name=%s" % args.geometry)
 
 
 
-outputarea=args.write
-hostarea=args.cdarea
-scriptarea=args.script
+outputarea=os.getcwd()+"/output/"
+hostarea=os.getcwd()+"/jobs/"
 
 
-print("output area is %s" % outputarea )
-print("host area is %s" % hostarea )
-print("script area is %s" % scriptarea )
+
 
 
 #nenergy=9
-#energies=[10,15,20,25,30,35,40,45,50]
-nenergy=1
-energies=[20]
+energies=[10,20,30]
+nenergy=len(energies)
+#energies=[20]
 name="condor-executable-"+str(args.geometry)+"-"
 direct="0. 0.0 1."
 poss=" 0. 0. -80.*cm"
@@ -64,22 +59,22 @@ print(poss)
 i=0
 while (i<nenergy):
     print(i)
-    shfile = open(scriptarea+name+str(energies[i])+'_GeV-e.sh',"w")
+    shfile = open(hostarea+args.geometry+"/"+name+str(energies[i])+'_GeV-e.sh',"w")
 
     shfile.write('#!/bin/bash'+'\n')
-    shfile.write('cd '+hostarea+'\n')
+    shfile.write('cd '+os.getcwd()+'/'+'\n')
     shfile.write('START_TIME=`/bin/date`'+'\n')
     shfile.write('echo "started at $START_TIME"'+'\n')
     shfile.write('echo "started at $START_TIME on ${HOSTNAME}"'+'\n')
-    shfile.write('singularity run -B /cvmfs:/cvmfs -B /data:/data docker://gitlab-registry.cern.ch/sft/docker/alma9-core:latest'+'\n')
+    #shfile.write('singularity run -B /cvmfs:/cvmfs -B /data:/data docker://gitlab-registry.cern.ch/sft/docker/alma9-core:latest'+'\n')
     shfile.write('source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc14-opt/setup.sh'+'\n')
     shfile.write('echo "ran setup"'+'\n')
-    shfile.write('source '+hostarea+'../install/bin/thisDualIanna.sh'+'\n')
-    shfile.write('echo "ran thisDualIanna"'+'\n')
+    shfile.write('source  /eos/user/a/adagarwa/DD4hep/examples/DualTestBeam/install/bin/thisDualTestBeam.sh'+'\n')
+    shfile.write('echo "ran thisDualTestBeam"'+'\n')
 # another good direction is  "0 0.05 0.99875"  and position 0.,-7*mm,-1*cm use this for pure fiber
 # DO IT BOTH PLACES!!!
-    shfile.write('ddsim --compactFile='+hostarea+'/DR'
-                 +str(args.geometry)+'.xml --runType=batch -G --steeringFile '+hostarea+'SCEPCALsteering.py --outputFile='+outputarea+'out_'+str(args.geometry)+'_'+str(energies[i])+'GeV_e-.root --part.userParticleHandler='' -G --gun.position="'+poss+'" --gun.direction "'+direct+'" --gun.energy "'+str(energies[i])+'*GeV" --gun.particle="e-" -N '+str(args.number)+' >& '+outputarea+'sce_e_'+str(args.geometry)+'_'+str(energies[i])+'.log'+'\n')
+    shfile.write('ddsim --compactFile='+os.getcwd()+'/DR'
+                 +str(args.geometry)+'.xml --runType=batch -G --steeringFile '+os.getcwd()+'/SCEPCALsteering.py --outputFile='+outputarea+args.geometry+'/out_'+str(args.geometry)+'_'+str(energies[i])+'GeV_e-.root --part.userParticleHandler='' -G --gun.position="'+poss+'" --gun.direction "'+direct+'" --gun.energy "'+str(energies[i])+'*GeV" --gun.particle="e-" -N '+str(args.number)+' >& '+outputarea+args.geometry+'/sce_e_'+str(args.geometry)+'_'+str(energies[i])+'.log'+'\n')
     shfile.write('exitcode=$?'+'\n')
     shfile.write('echo ""'+'\n')
     shfile.write('END_TIME=`/bin/date`'+'\n')
@@ -94,19 +89,19 @@ while (i<nenergy):
 i=0
 while (i<nenergy):
     print(i)
-    shfile = open(scriptarea+name+str(energies[i])+'_GeV-pi.sh',"w")
+    shfile = open(hostarea+args.geometry+"/"+name+str(energies[i])+'_GeV-pi.sh',"w")
 
     shfile.write('#!/bin/bash'+'\n')
-    shfile.write('cd '+hostarea+'\n')
+    shfile.write('cd '+os.getcwd()+'/'+'\n')
     shfile.write('START_TIME=`/bin/date`'+'\n')
     shfile.write('echo "started at $START_TIME"'+'\n')
     shfile.write('echo "started at $START_TIME on ${HOSTNAME}"'+'\n')
-    shfile.write('singularity run -B /cvmfs:/cvmfs -B /data:/data docker://gitlab-registry.cern.ch/sft/docker/alma9-core:latest'+'\n')
+    #shfile.write('singularity run -B /cvmfs:/cvmfs -B /data:/data docker://gitlab-registry.cern.ch/sft/docker/alma9-core:latest'+'\n')
     shfile.write('source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc14-opt/setup.sh'+'\n')
     shfile.write('echo "ran setup"'+'\n')
-    shfile.write('source '+hostarea+'../install/bin/thisDualIanna.sh'+'\n')
-    shfile.write('echo "ran thisDualIanna"'+'\n')
-    shfile.write('ddsim --compactFile='+hostarea+'/DR'+str(args.geometry)+'.xml --runType=batch -G --steeringFile '+hostarea+'/SCEPCALsteering.py --outputFile='+outputarea+'out_'+str(args.geometry)+'_'+str(energies[i])+'GeV_pi-.root --part.userParticleHandler='' -G --gun.position="'+poss+'" --gun.direction "'+direct+'" --gun.energy "'+str(energies[i])+'*GeV" --gun.particle="pi-" -N '+str(args.number)+' >& '+outputarea+'sce_pi_'+str(args.geometry)+'_'+str(energies[i])+'.log'+'\n')
+    shfile.write('source  /eos/user/a/adagarwa/DD4hep/examples/DualTestBeam/install/bin/thisDualTestBeam.sh'+'\n')
+    shfile.write('echo "ran thisDualTestBeam"'+'\n')
+    shfile.write('ddsim --compactFile='+os.getcwd()+'/DR'+str(args.geometry)+'.xml --runType=batch -G --steeringFile '+os.getcwd()+'/SCEPCALsteering.py --outputFile='+outputarea+args.geometry+'/out_'+str(args.geometry)+'_'+str(energies[i])+'GeV_pi-.root --part.userParticleHandler='' -G --gun.position="'+poss+'" --gun.direction "'+direct+'" --gun.energy "'+str(energies[i])+'*GeV" --gun.particle="pi-" -N '+str(args.number)+' >& '+outputarea+args.geometry+'/sce_pi_'+str(args.geometry)+'_'+str(energies[i])+'.log'+'\n')
     shfile.write('exitcode=$?'+'\n')
     shfile.write('echo ""'+'\n')
     shfile.write('END_TIME=`/bin/date`'+'\n')
@@ -122,18 +117,16 @@ while (i<nenergy):
 i=0
 while (i<nenergy):
     print(i)
-    jdlfile = open(scriptarea+name+str(energies[i])+'-e.jdl',"w")
-    jdlfile.write("universe = vanilla"+'\n')
-    jdlfile.write("Executable ="+scriptarea+name+str(energies[i])+"_GeV-e.sh"+'\n')
-    jdlfile.write("should_transfer_files = NO"+'\n')
-    jdlfile.write("Requirements = machine == \"hepcms-henrietta.privnet\""+'\n')
+    jdlfile = open(hostarea+args.geometry+"/"+name+str(energies[i])+'-e.jdl',"w")
+    jdlfile.write("Executable = "+name+str(energies[i])+"_GeV-e.sh"+'\n')
+#    jdlfile.write("Requirements = machine == \"hepcms-henrietta.privnet\""+'\n')
 #    jdlfile.write("request_memory = 15GB"+'\n')
 #    jdlfile.write("RequestCpus = 4"+'\n')
-    jdlfile.write("Output = "+outputarea+name+str(energies[i])+"-e_sce_$(cluster)_$(process).stdout"+'\n')
-    jdlfile.write("Error = "+outputarea+name+str(energies[i])+"-e_sce_$(cluster)_$(process).stderr"+'\n')
-    jdlfile.write("Log = "+outputarea+name+str(energies[i])+"-e_sce_$(cluster)_$(process).condor"+'\n')
-    jdlfile.write("Arguments = SCE"+'\n')
-    jdlfile.write("Queue 1"+'\n')
+    jdlfile.write("Output = "+name+str(energies[i])+"-e_sce_$(cluster)_$(process).stdout"+'\n')
+    jdlfile.write("Error = "+name+str(energies[i])+"-e_sce_$(cluster)_$(process).stderr"+'\n')
+    jdlfile.write("Log = "+name+str(energies[i])+"-e_sce_$(cluster)_$(process).condor"+'\n')
+    jdlfile.write("+JobFlavour = \"tomorrow\"" + '\n')
+    jdlfile.write("Queue "+'\n')
     jdlfile.close()
     i=i+1
     print("file closed")
@@ -142,18 +135,17 @@ while (i<nenergy):
 i=0
 while (i<nenergy):
     print(i)
-    jdlfile = open(scriptarea+name+str(energies[i])+'-pi.jdl',"w")
-    jdlfile.write("universe = vanilla"+'\n')
-    jdlfile.write("Executable ="+scriptarea+name+str(energies[i])+"_GeV-pi.sh"+'\n')
-    jdlfile.write("should_transfer_files = NO"+'\n')
-    jdlfile.write("Requirements = machine == \"hepcms-henrietta.privnet\""+'\n')
+    jdlfile = open(hostarea+args.geometry+"/"+name+str(energies[i])+'-pi.jdl',"w")
+    #jdlfile.write("universe = vanilla"+'\n')
+    jdlfile.write("Executable = "+name+str(energies[i])+"_GeV-pi.sh"+'\n')
+    #jdlfile.write("Requirements = machine == \"hepcms-henrietta.privnet\""+'\n')
 #    jdlfile.write("request_memory = 15GB"+'\n')
 #    jdlfile.write("RequestCpus = 4"+'\n')
-    jdlfile.write("Output = "+outputarea+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).stdout"+'\n')
-    jdlfile.write("Error = "+outputarea+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).stderr"+'\n')
-    jdlfile.write("Log = "+outputarea+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).condor"+'\n')
-    jdlfile.write("Arguments = SCE"+'\n')
-    jdlfile.write("Queue 1"+'\n')
+    jdlfile.write("Output = "+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).stdout"+'\n')
+    jdlfile.write("Error = "+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).stderr"+'\n')
+    jdlfile.write("Log = "+name+str(energies[i])+"-pi_sce_$(cluster)_$(process).condor"+'\n')
+    jdlfile.write("+JobFlavour = \"tomorrow\""+'\n')
+    jdlfile.write("Queue "+'\n')
     jdlfile.close()
     i=i+1
     print("file closed")
@@ -161,12 +153,13 @@ while (i<nenergy):
 
 
 # create the submitter file
+#os.chdir("~/DD4hep/examples/DualTestBeam/compact/jobs/"+args.geometry+"/")
 f = open("massjobs.sh",'w')
-f.write('chmod 777 '+scriptarea+'*'+'\n')
+f.write('chmod 777 '+hostarea+'*'+'\n')
 i=0
 while (i<nenergy):
-    f.write("condor_submit "+scriptarea+name+str(energies[i])+'-e.jdl'+'\n')
-    f.write("condor_submit "+scriptarea+name+str(energies[i])+'-pi.jdl'+'\n')
+    f.write("condor_submit "+name+str(energies[i])+'-e.jdl'+'\n')
+    f.write("condor_submit "+name+str(energies[i])+'-pi.jdl'+'\n')
     i=i+1
 f.write("condor_q")
 f.close()
