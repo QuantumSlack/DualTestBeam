b2f4c582c11648d46f7fa016605ade3744eca6d2
atlas <atlas@ibl-ts.physics.sunysb.edu>
2025-09-20 17:26:25 -0400

    Updated the .sh file

--- compact/analysis/out_to_hist.sh
+++ compact/analysis/out_to_hist.sh
@@ -2,15 +2,20 @@
 number_particles=$1
 geometry=$2
 energy=$3
-hcaltype=$4
-leaf1=$5
-leaf2=$6
+doecal=$4
+dohcal=$5
+hcaltype=$6
+leaf1=$7
+leaf2=$8
 CWD="$(realpath "$(pwd)/..")"
 INPUT_FILE_DIR="$CWD/output/$geometry"
 OUT_FILE_DIR="$CWD/output/$geometry"
 LOG_FILE_DIR="$CWD/output/$geometry"
-input_string="Resolution($number_particles,"\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_e-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_pi-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_e-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_pi-.root"\","$energy",0,1,$hcaltype,1,0,0,0,3,3,"\""$OUT_FILE_DIR"/hists_"$energy"GeV_"$geometry"_trial.root"\","\""$leaf1""\","\""$leaf2""\",0,0,0,0)"
+input_string="Resolution($number_particles,"\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_e-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_pi-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_e-.root"\","\""$INPUT_FILE_DIR"/out_"$geometry"_"$energy"GeV_pi-.root"\","$energy","$doecal","$dohcal",$hcaltype,1,0,0,0,3,3,"\""$OUT_FILE_DIR"/hists_"$energy"GeV_"$geometry"_trial.root"\","\""$leaf1""\","\""$leaf2""\",1,0,1,1)"
 #echo $input_string
+if [ -f "runResolution.cpp" ]; then
+	rm -rf runResolution.cpp
+fi
 echo '
 #include "include/Resolution.h"
 	int main(){
